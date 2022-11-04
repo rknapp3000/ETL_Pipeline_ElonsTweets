c56b26f19876729b8048c421cc265633a526e6c3
ryan  knapp <ryanknapp@ryans-mbp.myfiosgateway.com>
2022-11-03 22:15:10 -0400

    updates to date and interval setting

--- dag.py
+++ dag.py
@@ -8,7 +8,7 @@ from twitter_etl_pipeline import run_twitter_etl
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2020, 11, 8),
+    'start_date': datetime(2022, 11, 3),
     'email': ['ryanknapp56@gmail.com'],
     'email_on_failure': False,
     'email_on_retry': False,
@@ -16,3 +16,9 @@ default_args = {
     'retry_delay': timedelta(minutes=1)
 }
 
+dag = DAG(
+    'twitter_dag',
+    default_args=default_args,
+    description='Twitter Dag ETL Process',
+    schedule_interval=timedelta(days=1),
+)

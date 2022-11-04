3baece0519664221d7e6145e7121a7d421507878
ryan  knapp <ryanknapp@ryans-mbp.myfiosgateway.com>
2022-11-03 22:11:35 -0400

    created dag python file

---
+++ dag.py
@@ -0,0 +1,18 @@
+from datetime import timedelta
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.utils.dates import days_ago
+from datetime import datetime
+from twitter_etl_pipeline import run_twitter_etl
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2020, 11, 8),
+    'email': ['ryanknapp56@gmail.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1)
+}
+
